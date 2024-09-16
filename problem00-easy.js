777c8e9eef1a3e91e82b866093ca22edfea5ab44
Jessica Garcia <webdev.jessgarcia@gmail.com>
2024-09-16 16:59:38 -0300

    Problem00-easy-solved

---
+++ problem00-easy.js
@@ -0,0 +1,12 @@
+//Write a JavaScript function that takes two numbers as input and returns their sum.
+
+//Requirements:
+//You need to create a function called sum.
+//The function should accept two parameters: num1 and num2.
+//Return the sum of num1 and num2.
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(4, 5));
