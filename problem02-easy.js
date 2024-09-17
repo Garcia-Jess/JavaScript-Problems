c2f8473d442838a9f22c1f280de834a411bccd31
Jessica Garcia <webdev.jessgarcia@gmail.com>
2024-09-17 20:35:32 -0300

    Problem02-easy-solved

---
+++ problem02-easy.js
@@ -0,0 +1,13 @@
+//Problem 02: Convert Minutes to Seconds
+//Write a JavaScript function that converts a given number of minutes into seconds.
+
+//Requirements:
+//Create a function called convertToSeconds.
+//The function should accept one parameter: minutes.
+//Return the number of seconds in the given minutes (i.e., minutes * 60).
+
+function convertToSeconds(minutes) {
+  return minutes * 60;
+}
+
+console.log(convertToSeconds(5));
