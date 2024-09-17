ec2d627db439c255a4ef8d27796106cb1fa4aa13
Jessica Garcia <webdev.jessgarcia@gmail.com>
2024-09-17 07:09:42 -0300

    Problem01-easy-solved

---
+++ problem01-easy.js
@@ -0,0 +1,13 @@
+//Problem 01: Square a Number
+//Write a JavaScript function that takes a number as input and returns the square of that number.
+
+//Requirements:
+//Create a function called square.
+//The function should accept one parameter: num.
+//Return the square of num (i.e., num * num).
+
+function square(num) {
+  return num * num;
+}
+
+console.log(square(3));
