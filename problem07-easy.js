deaccf765f585741341e760ae30427f4b61254f4
Jessica Garcia <webdev.jessgarcia@gmail.com>
2024-09-19 07:52:04 -0300

    Problem07-easy-solved

---
+++ problem07-easy.js
@@ -0,0 +1,8 @@
+//Problema 08 - Super Easy:
+//Escreva uma função chamada multiply que recebe dois números como argumentos e retorna o produto (multiplicação) desses dois números.
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(3, 4));
