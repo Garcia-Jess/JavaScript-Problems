220eb9623e1551f1e2f0f5d87c2fb7d6b443af9b
Jessica Garcia <webdev.jessgarcia@gmail.com>
2024-09-19 06:31:18 -0300

    Problem06-easy-solved

---
+++ problem06-easy.js
@@ -0,0 +1,8 @@
+//Problema 07 - Super Easy:
+//Escreva uma função chamada greet que recebe um nome como argumento e retorna uma saudação com o nome fornecido.
+
+function greet(name) {
+  return `Hello, ${name}`;
+}
+
+console.log(greet("Jessica"));
